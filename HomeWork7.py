293b013a02d5676fd90d2c4c2afe2d6a7591f8b1
VovkAnna <69368079+VovkAnna@users.noreply.github.com>
2020-08-19 17:22:48 +0300

    Add files via upload

--- HomeWork7.py
+++ HomeWork7.py
@@ -1,3 +1,34 @@
-my_list =[]
-for val in range(0,100):
-    my_list
+import random
+my_list=[]
+while len(my_list) <= 20:
+    a=random.randint(0,100)
+    my_list.append(a)
+print(my_list)
+###########################################################
+import random
+triangle ={"A":(),
+           "B": (),
+           "C": ()}
+triangle["A"] = (random.randint(-100,100),random.randint(-100,100))
+triangle["B"] = (random.randint(-100,100),random.randint(-100,100))
+triangle["C"] = (random.randint(-100,100),random.randint(-100,100))
+print(triangle)
+###########################################################
+def my_print(strA):
+    print("***"+strA+"***")
+my_str="Juliette has a gun"
+my_print(my_str)
+##########################################################
+def my_print(strA):
+    a = len(strA)*"*"
+    print(f"{a}\n{strA}\n{a}")
+my_str ="Juliette has a gun"
+my_print(my_str)
+#########################################################
+def my_print(strA):
+    a = len(strA)*"*"
+    print(f"{a}\n{strA}\n{a}")
+my_str ="Juliette has a gun"
+my_str1 = "***"+my_str+"***"
+my_print(my_str1)
+########################################################
